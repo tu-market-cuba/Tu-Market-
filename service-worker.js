1982d65a48f23c6698ee0a1a2b32846f8e829740
tu-market-cuba <borisjaniel95@gmail.com>
2026-08-23 14:03:19 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tu-market-offline-v1';
+const CACHE='tu-market-offline-v2';
 const FILES=['./','./index.html','./admin.html','./tu-market.css','./tu-market-brand.jpg','./tu-market-logo-realistic.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
